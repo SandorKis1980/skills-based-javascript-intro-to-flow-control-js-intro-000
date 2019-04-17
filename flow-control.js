a2914ce7d4e0cee3ed75511281ae3c0b32afa456
organized-counter-5921 <kalapacs@gmail.com>
2019-04-17 22:17:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,6 +12,12 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
+  if (age>=13 && age <= 19) {
+    return "You are a teenager!";
+  } else if (age < 13) {
+      return "You are not a teenager";
+    } 
+    return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
